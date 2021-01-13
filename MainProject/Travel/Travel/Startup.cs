dcf8262e1cee443a5fb606de87b61e5993666336
SvorotnevaNatasha <n.svorotneva@gmail.com>
2021-01-13 20:08:57 +0300

    start page

--- MainProject/Travel/Travel/Startup.cs
+++ MainProject/Travel/Travel/Startup.cs
@@ -54,7 +54,7 @@ namespace Travel
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Places}/{action=Index}/{id?}");
             });
         }
     }
