09f541e5a6b776bffca23b28232af31474edbb5e
DrWilton <ethanchest48@gmail.com>
2021-01-14 17:33:45 +0300

    Add Identity(файлы для регистрации)

---
+++ MainProject/Travel/Travel/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Travel.Data;
+
+[assembly: HostingStartup(typeof(Travel.Areas.Identity.IdentityHostingStartup))]
+namespace Travel.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<TravelContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("TravelContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<TravelContext>();
+            });
+        }
+    }
+}
