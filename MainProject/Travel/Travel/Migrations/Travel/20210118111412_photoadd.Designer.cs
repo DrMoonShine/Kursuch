2130ca7ea04dbb2e8aa5466ed960d25e31eaeae7
DrWilton <ethanchest48@gmail.com>
2021-01-18 14:14:58 +0300

    Photo update v2.0

--- MainProject/Travel/Travel/Migrations/Travel/20210118111412_photoadd.Designer.cs
+++ MainProject/Travel/Travel/Migrations/Travel/20210118111412_photoadd.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Travel.Data;
 
 namespace Travel.Migrations.Travel
 {
     [DbContext(typeof(TravelContext))]
-    partial class TravelContextModelSnapshot : ModelSnapshot
+    [Migration("20210118111412_photoadd")]
+    partial class photoadd
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MainProject/Travel/Travel/Migrations/Travel/20210118111412_photoadd.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Travel.Migrations.Travel
+{
+    public partial class photoadd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Photo",
+                table: "Place",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Place");
+        }
+    }
+}
